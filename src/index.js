79f3814b54b2dd990dd8cb086cdd07aad0188524
xperience001 <tobilobatijani2011@gmail.com>
2023-03-17 03:47:09 +0100

    key

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
+const ErrorBoundary = Bugsnag.getPlugin('react')
+  .createErrorBoundary(React)
+
+  Bugsnag.start({
+    apiKey: 'b29b5653b31840058c490776d3f9ad5f',
+    plugins: [new BugsnagPluginReact()]
+  })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
